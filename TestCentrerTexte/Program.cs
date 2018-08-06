5e1899ea2a565657ad7efa0516c2e03667b07450
AlexinaO <alexina.orsoni@gmail.com>
2018-08-06 11:33:09 +0200

    Test Centrer texte

--- TestCentrerTexte/Program.cs
+++ TestCentrerTexte/Program.cs
@@ -10,6 +10,15 @@ namespace TestCentrerTexte
     {
         static void Main(string[] args)
         {
+            CentrerLeTexte("Hello world");
+            CentrerLeTexte("Je suis un texte plus long");
+        }
+
+        private static void CentrerLeTexte(string texte)
+        {
+            int nbEspaces = (Console.WindowWidth - texte.Length) / 2;
+            Console.SetCursorPosition(nbEspaces, Console.CursorTop);
+            Console.WriteLine(texte);
         }
     }
 }
